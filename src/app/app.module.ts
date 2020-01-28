7a86d72957e170a3e6e990e227c5caa275b75764
Phil <philbroderick12@gmail.com>
2020-01-28 17:53:40 +0000

    Add navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { ShowComponent } from './components/show/show.component';
 import { ShowDetailComponent } from './components/show-detail/show-detail.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ShowComponent,
     ShowDetailComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
