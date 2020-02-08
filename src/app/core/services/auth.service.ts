aec5ba459b21ccdf24b816b9908080307b2a7da4
Phil <philbroderick12@gmail.com>
2020-02-08 20:04:34 +0000

    Allow user to log in and out

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { environment } from "./../../../environments/environment";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { map } from "rxjs/operators";
 
 const httpOptions = {
   headers: new HttpHeaders({ "Content-Type": "application/json" })
@@ -10,20 +11,29 @@ const httpOptions = {
   providedIn: "root"
 })
 export class AuthService {
-  authUrl = environment.baseApiUrl + "auth";
+  authUrl = environment.baseApiUrl + "auth/";
 
   constructor(private http: HttpClient) {}
 
   login = (username: string, password: string) => {
-    var token = this.http.post(this.authUrl + "login", {
-      username: username,
-      password: password
-    });
-    console.log(token);
-    return token;
+    return this.http
+      .post(this.authUrl + "login", {
+        username: username,
+        password: password
+      })
+      .pipe(
+        map((response: any) => {
+          const user = response;
+          if (user) {
+            localStorage.setItem("token", user.token);
+            sessionStorage.setItem("loggedUser", username);
+          }
+        })
+      );
   };
 
-  logout = () => localStorage.removeItem("token");
-
-  isLoggedIn = () => !!localStorage.getItem("token");
+  logout = () => {
+    localStorage.removeItem("token");
+    sessionStorage.removeItem("loggedUser");
+  };
 }
